38bff218ba823fb8d5b6e50d63fe83597e42a07d
Mark Keeble <markkeeble14@gmail.com>
2022-10-06 09:10:11 -0700

    initial commit

---
+++ app/src/main/java/com/example/comp7082_lab_exercise_19_1102/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.comp7082_lab_exercise_19_1102;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
